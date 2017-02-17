6ef5d6b21c1ea2572669acfc3d30d86cca52e522
Дарья Соловей < >
2017-02-17 22:17:22 +0300

    Camera without permission

---
+++ app/src/main/java/com/example/butterfly/camera_without_permission/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.butterfly.camera_without_permission;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    File directory;
+    final int TYPE_PHOTO = 1;
+    final int TYPE_VIDEO = 2;
+
+    final int REQUEST_CODE_PHOTO = 1;
+    final int REQUEST_CODE_VIDEO = 2;
+
+    final String TAG = "myLogs";
+
+    Button photo, video;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createDirectory();
+
+        photo = (Button) findViewById(R.id.photo);
+        video = (Button) findViewById(R.id.video);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, generateFileUri(TYPE_PHOTO));
+                startActivityForResult(intent, REQUEST_CODE_PHOTO);
+            }
+        });
+
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, generateFileUri(TYPE_VIDEO));
+                startActivityForResult(intent, REQUEST_CODE_VIDEO);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_CODE_PHOTO) {
+            if (resultCode == RESULT_OK) {
+                if (intent == null) {
+                    Log.d(TAG, "Intent is null");
+                } else {
+                    Log.d(TAG, "Photo uri: " + intent.getData());
+                    Bundle bundle = intent.getExtras();
+                    if (bundle != null) {
+                        Object obj = intent.getExtras().get("data");
+                        if (obj instanceof Bitmap) {
+                            Bitmap bitmap = (Bitmap) obj;
+                            Log.d(TAG, "bitmap " + bitmap.getWidth() + " x "+ bitmap.getHeight());
+                            imageView.setImageBitmap(bitmap);
+                        }
+                    }
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.d(TAG, "Canceled");
+            }
+        }
+
+        if (requestCode == REQUEST_CODE_VIDEO) {
+            if (resultCode == RESULT_OK) {
+                if (intent == null) {
+                    Log.d(TAG, "Intent is null");
+                } else {
+                    Log.d(TAG, "Video uri: " + intent.getData());
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.d(TAG, "Canceled");
+            }
+        }
+    }
+
+    private Uri generateFileUri(int type) {
+        File file = null;
+        switch (type) {
+            case TYPE_PHOTO:
+                file = new File(directory.getPath() + "/" + "photo_"+ System.currentTimeMillis() + ".jpg");
+                break;
+            case TYPE_VIDEO:
+                file = new File(directory.getPath() + "/" + "video_" + System.currentTimeMillis() + ".mp4");
+                break;
+        }
+        Log.d(TAG, "fileName = " + file);
+        return Uri.fromFile(file);
+    }
+
+    private void createDirectory() {
+        directory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"MyFolder");
+        if (!directory.exists())
+            directory.mkdirs();
+    }
+
+}
